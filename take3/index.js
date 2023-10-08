35f0aff9e64675ee2f1a6a50a8c84478404d15a5
echocslsk <CardioLeo@gmail.com>
2023-10-08 15:11:38 -0600

    incomplete re-typing of the index.js and .html docs

---
+++ take3/index.js
@@ -0,0 +1,95 @@
+class Book {
+	constructor(title, author, topic, progress) {
+		this.title = title;
+		this.author = author;
+		this.progress = progress;
+	}
+
+	describe() {
+		return '${this.title} is written by ${this.author}. I am ${this.progress}% through the book.';
+	}
+}
+
+class Shelf {
+	constructor(shelf) {
+		this.shelf = shelf;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book instanceof Book) {
+			this.shelf.push(book)
+		} else {
+			throw new Error('You can only add an instance of the Book class. Argument is not a Book: ${book}');
+		}
+	}
+	describe() {
+		return '${this.shelf} has ${this.shelf.length} books.';
+	}	
+}
+
+class Menu {
+	constructor() {
+		this.shelves = [];
+		this.selectedShelf = null;
+	}
+
+	start() {
+		let selection = this.showMainMenuOptions();
+		
+		while (selection != 0) {
+			switch (selection) {
+				case '1':
+					this.createShelf();
+					break;
+				case '2':
+					this.viewShelf();
+					break;
+				case '3':
+					this.deleteShelf();
+					break;
+				case '4':
+					this.displayShelves();
+					break;
+				default:
+					selection = 0;
+			}
+			selection = this.showMainMenuOptions();
+		}
+		alert('Goodbye!');
+	}
+	showMainMenuOptions() {
+		return prompt('
+			0) exit
+			1) create new shelf
+			2) view shelf
+			3) delete shelf
+			4) display all shelves
+		');
+	}
+
+	displayShelves() {
+		let shelfString = '';
+		for (let i = 0; i < this.shelves.length; i++) {
+			shelfString += i + ') ' + this.shelves[i].shelf + '\n';
+		}
+		alert(shelfString);
+	}
+
+	createShelf() {
+		let shelf = prompt('Enter a topic for this new shelf:')
+		this.shelves.push(new Shelf(shelf));
+	}
+
+	viewShelf() {
+		let index = prompt('Enter the index of the shelf you wish to view:');
+		if (index > -1 && index < this.shelves.length) {
+			this.selectedShelf = this.shelves[index];
+			let description = 'Shelf Description: ' + this.selectedShelf.shelf + '\n';
+
+			for (let i = 0; i < this.selectedShelf.books.length; i++) {
+				description += i + ') ' + this.selectedShelf.shelf[i].title = '\n';
+			}
+		}
+	}
+}
